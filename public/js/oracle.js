2bc11697abbe43e58563e04bae1bf47c020b2eda
choikj129 <ckj9829@naver.com>
2022-12-14 00:15:34 +0900

    feat : db 연동

---
+++ public/js/oracle.js
@@ -0,0 +1,5 @@
+module.exports = {
+    user          : "ODN_GJCHOI",
+    password      : "m1m2m3",
+    connectString : "192.168.10.12:41522/XE",
+};
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,19 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+const { password } = require('../public/js/oracle');
+let router = express.Router();
+let dbconfig = require("../public/js/oracle")
+let db = require("oracledb");
+
+db.initOracleClient({libDir:"C:\\oracle\\instantclient_21_7"})
+db.outFormat = db.OUT_FORMAT_OBJECT
+
+let conn;
+db.getConnection(
+  dbconfig,
+  function(err, connection){
+    conn = connection
+  }
+)
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,7 +21,10 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-  console.log("#######################")
+  conn.execute("select 1 from dual", {}, function(err, res){
+    console.log(res)
+  })
+  
   res.json({ title: 'Express' });
 });
 
