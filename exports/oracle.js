00f07cbf0c5788f6826c1507aa534524004a79d7
choikj129 <ckj9829@naver.com>
2023-01-22 01:20:13 +0900

    feat : multi update 기능 추가

--- exports/oracle.js
+++ exports/oracle.js
@@ -8,18 +8,18 @@ let config = require("./config/db_connect")
     }
 */
 
-db.initOracleClient({libDir:"C:\\oracle\\instantclient_21_8"})
+db.initOracleClient({ libDir: "C:\\oracle\\instantclient_21_8" })
 db.outFormat = db.OUT_FORMAT_OBJECT
 let conn = null;
 module.exports = {
-    connection : (callback) => {
+    connection: (callback) => {
         db.getConnection(
             {
-                user          : config.user,
-                password      : config.password,
-                connectString : config.connectString,
+                user: config.user,
+                password: config.password,
+                connectString: config.connectString,
             },
-            (err, connection) =>{
+            (err, connection) => {
                 if (err) {
                     console.log("DB connection error")
                     console.error(err)
@@ -30,50 +30,96 @@ module.exports = {
                     callback(true)
                 }
             }
-            )
-        },
-        close : () => {
-            console.log("DB Close")
-            conn.close()
-        },
-        select : (query, params, callback) => {        
-        conn.execute(query, params, (err, result) =>{
-            if (err){
+        )
+    },
+    close: () => {
+        console.log("DB Close")
+        conn.close()
+    },
+    select: (query, params, callback) => {
+        conn.execute(query, params, (err, result) => {
+            if (err) {
                 console.log("DB select error")
+                console.log("==========================================================")
+                console.log(query)
+                console.log("==========================================================")
                 console.error(err)
                 callback(false)
-            }else{                
-                callback(true, result.rows)                
+            } else {
+                callback(true, result.rows)
             }
         })
     },
-    update : (query, params, callback) => {
-        conn.execute(query, params, (err, result) =>{
-            if (err){
-                console.log("DB update error")
-                console.error(err)
-                callback(false)
-            }else{                
-                callback(true, result.rowsAffected)                
-            }
-        })
+    // multiSelect : (hash, callback) => {
+        
+    // },
+    update: (query, params, callback) => {
+        if (params.length > 0) {
+            conn.execute(query, params, (err, result) => {
+                if (err) {
+                    console.log("DB update error")
+                    console.log("==========================================================")
+                    console.log(query)
+                    console.log("==========================================================")
+                    console.error(err)
+                    callback(false)
+                } else {
+                    callback(true, result.rowsAffected)
+                }
+            })
+        } else {
+            callback(true, 0)
+        }
     },
-    updateBulk : (query, params, callback) => {
-        conn.executeMany(query, params, (err, result) =>{
-            if (err){
-                console.log("DB update bulk error")
-                console.error(err)
-                callback(false)
-            }else{                
-                callback(true, result.rowsAffected)
-            }
-        })
+    updateBulk: (query, params, callback) => {
+        if (params.length > 0) {
+            conn.executeMany(query, params, (err, result) => {
+                if (err) {
+                    console.log("DB update bulk error")
+                    console.log("==========================================================")
+                    console.log(query)
+                    console.log("==========================================================")
+                    console.error(err)
+                    callback(false)
+                } else {
+                    callback(true, result.rowsAffected)
+                }
+            })
+        }else {
+            callback(true, 0)
+        }
     },
-    commit : () => {
+    multiUpdateBulk : (hash, callback) => {
+        keys = Object.keys(hash)
+        if (keys.length > 0) {
+            returnData = {}
+            keys.forEach((key, i) => {
+                conn.executeMany(hash[key].query, hash[key].params, (err, result) => {
+                    if (err) {
+                        console.log("DB multi update bulk error")
+                        console.log("==========================================================")
+                        console.log(hash[key].query)
+                        console.log("==========================================================")
+                        console.error(err)
+                        callback(false)
+                        return false;
+                    } else {
+                        returnData[key] = result.rowsAffected
+                        if (i == keys.length -1) {
+                            callback(true, returnData)
+                        }
+                    }
+                })
+            })
+        } else {
+            callback(true, 0)
+        }
+    },
+    commit: () => {
         console.log("DB commit")
         conn.commit()
     },
-    rollback : () => {
+    rollback: () => {
         console.log("DB rollback")
         conn.rollback()
     },
