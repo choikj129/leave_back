6bf741a3ca1452d1cb72ab9d7123fdf12e19dd9a
choikj129 <ckj9829@naver.com>
2023-02-14 17:22:14 +0900

    feat : 일정 삭제 개선

--- routes/leave.js
+++ routes/leave.js
@@ -104,33 +104,33 @@ router.post('/', (req, res, next) => {
                                     : dbHash.leaveDetailDelete
                                 dbHash.leaveDelete.params.push([param.IDX])
                                 dbHash.leaveDetailDelete.params.push([param.IDX])
-                                param.name += " 취소"
                             }
                             kakaoWorkArr.push(param.name)
 
                         }
                         db.multiUpdateBulk(conn, dbHash, (succ, result) => {
                             if (succ) {
-                                funcs.sendSuccess(res, [], "휴가 등록 / 취소 완료")
-                                db.commit(conn)
-                                db.close(conn)
+                                // funcs.sendSuccess(res, [], "휴가 등록 / 취소 완료")
+                                // db.commit(conn)
+                                // db.close(conn)
+
                                 /* 워크로 휴가 신청 정보 전송 */
-                                // if (req.session.user.isManager) {
-                                //     funcs.sendSuccess(res, [], "휴가 등록 / 취소 완료")
-                                //     db.commit(conn)
-                                //     db.close(conn)
-                                // } else {
-                                //     kakaowork.sendMessage(kakaoWorkArr.sort(), req.session.user, (isSend) => {
-                                //         if (isSend) {
-                                //             funcs.sendSuccess(res, [], "카카오워크 전송 성공")
-                                //             db.commit(conn)
-                                //         } else {                                                            
-                                //             funcs.sendFail(res, "카카오워크 전송 실패")
-                                //             db.rollback(conn)
-                                //         }
-                                //         db.close(conn)
-                                //     })
-                                // }
+                                if (req.session.user.isManager) {
+                                    funcs.sendSuccess(res, [], "휴가 등록 / 취소 완료")
+                                    db.commit(conn)
+                                    db.close(conn)
+                                } else {
+                                    kakaowork.sendMessage(kakaoWorkArr.sort(), req.session.user, (isSend) => {
+                                        if (isSend) {
+                                            funcs.sendSuccess(res, [], "카카오워크 전송 성공")
+                                            db.commit(conn)
+                                        } else {                                                            
+                                            funcs.sendFail(res, "카카오워크 전송 실패")
+                                            db.rollback(conn)
+                                        }
+                                        db.close(conn)
+                                    })
+                                }
                             } else {
                                 funcs.sendFail(res, "휴가 등록 / 취소 실패")
                                 db.close(conn)
