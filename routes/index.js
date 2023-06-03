1cb4a63a09507c1efa6e894e10567769eff42149
choikj129 <ckj9829@naver.com>
2023-06-03 19:29:30 +0900

    fix : [get /birthday] code refactoring

--- routes/index.js
+++ routes/index.js
@@ -55,32 +55,29 @@ router.get("/birthday", async (req, res, next) => {
 	let conn
 	try {
 		conn = await db.connection()
-		const selectBirthday = `
+		const sql = `
 			SELECT 
 				이름 || ' ' || 직위 이름,
 				생일,
 				음력여부
-			FROM EMP_POS`
-		const selectKey = `SELECT 표시내용 KEY FROM CODE WHERE 코드구분 = '공공데이터키' AND 사용여부 = 'Y'`
+			FROM EMP_POS
+			WHERE 생일 IS NOT NULL
+		`
 		
-		const result = await db.multiSelect(conn, {
-			birthdays : {query : selectBirthday, params : {}},
-			apiKey : {query : selectKey, params : {}},
-		})
+		const result = await db.select(conn, sql, {})
 
-		
 		const year = new Date().getFullYear()				
 		
 		let birthdays = []
-		for (i=0; i<result.birthdays.length; i++) {
-			let birthday = result.birthdays[i]
+		for (i=0; i<result.length; i++) {
+			let birthday = result[i]
 			if (!birthday.생일) continue
 			if (birthday.음력여부 == 'N') {
 				let month = birthday.생일.substring(0, 2)
 				let day = birthday.생일.substring(2, 4)
 				for (let n=-1; n<2; n++) {
 					birthdays.push({
-						내용 : `${birthday.이름} ${month}월 ${day}일 생일`,
+						내용 : `${birthday.이름} ${month}월 ${day}일 생일🎉`,
 						생일 : `${(year + n)}-${month}-${day}`,
 						음력여부 : false
 					})
@@ -89,29 +86,22 @@ router.get("/birthday", async (req, res, next) => {
 				let month = birthday.생일.substring(0, 2)
 				let day = birthday.생일.substring(2, 4)
 				for (let n=-1; n<2; n++) {
+					let solar
 					try {
-						let solar = await holidayKR.getSolar(year + n, month, day, true)
-						console.log(birthday.이름, solar, true)
-						let solarMonth = solar.month < 10 ? "0" + solar.month : solar.month.toString()
-						let solarDay = solar.dat < 10 ? "0" + solar.dat : solar.dat.toString()
-						birthdays.push({
-							내용 : `${birthday.이름} 음력 ${month}월 ${day}일 생일`,
-							생일 : `${solar.year}-${solarMonth}-${solarDay}`,
-							음력여부 : true,
-						})
+						solar = await holidayKR.getSolar(year + n, month, day, true)
 					} catch {
 						try {
-							let solar = await holidayKR.getSolar(year + n, month, day, false)
-							console.log(birthday.이름, solar, false)
+							solar = await holidayKR.getSolar(year + n, month, day, false)
+						} catch {}
+					} finally {
+						if (solar) {
 							let solarMonth = solar.month < 10 ? "0" + solar.month : solar.month.toString()
 							let solarDay = solar.day < 10 ? "0" + solar.day : solar.day.toString()
 							birthdays.push({
-								내용 : `${birthday.이름} 음력 ${month}월 ${day}일 생일`,
+								내용 : `${birthday.이름} 음력 ${month}월 ${day}일 생일🎉`,
 								생일 : `${solar.year}-${solarMonth}-${solarDay}`,
 								음력여부 : true,
 							})
-						} catch {
-							console.log()
 						}
 					}
 				}
